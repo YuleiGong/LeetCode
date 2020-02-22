4b0c6a2d82461410be27a08338b7dee13089eec2
flex <542711662@qq.com>
2020-02-22 21:14:46 +0800

    add mindnode

---
+++ 144/144-my.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ylgongPw @ 2020-02-22 20:23:16
+
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        """
+        前序遍历首先访问根节点，然后遍历左子树，最后遍历右子树。
+        递归
+        """
+        vals = []
+        def helper(root):
+            if not root:
+                return
+            val = root.val
+            vals.append(val)
+            helper(root.left)
+            helper(root.right)
+
+        helper(root)
+        return vals
+
+if __name__ == '__main__':
+    alist = [1,None,2,3]
+    t = TreeNode(1)
+    t.right = TreeNode(2)
+    t.right.left = TreeNode(3)
+
+    S = Solution()
+    ret = S.preorderTraversal(t)
+    print (ret)
+
+
