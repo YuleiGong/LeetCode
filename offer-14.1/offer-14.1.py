88e361ae62a85a5dd0ddb432cc112f7f72ed3aa1
flex <542711662@qq.com>
2020-03-16 12:14:51 +0800

    add offer14

--- offer-14.1/offer-14.1.py
+++ offer-14.1/offer-14.1.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ylgongPw @ 2020-03-09 16:52:32
-from __future__ import unicode_literals
-from __future__ import absolute_import
 
 class Solution:
     def cuttingRope(self, n: int) -> int:
-        length = n
-        splits = n #最大
+        if n <= 3:
+            return n-1
+        val = n // 3
+        other = n % 3
+        if other == 1:
+            val = val - 1
+            return (3**val * (2 * 2))
+        elif other == 2:
+            return (3**val*2)
+        else:
+            return (3**val)
 
-        for i in range(2,n+1):
-            
 
 
+if __name__ == '__main__':
+    S = Solution()
+    ret = S.cuttingRope(5)
+    print (ret)
